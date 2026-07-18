dedd27b30d29fbeba200d73081ceb3a99ab2df3f
phuhoag <11a8hoangkimquyphu@gmail.com>
2026-07-18 16:26:43 +0700

    deloy test

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/database.js";
 import dotenv from "dotenv";
 import routers from "./routers/index.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -10,14 +11,27 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 // middleware;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 routers(app);
+
+// frontend;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("hello");
